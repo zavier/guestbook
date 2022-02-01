ef3b694f55b76b42993dd6a89ff0aa721c609e68
zheng <765324639@qq.com>
2022-02-01 22:43:12 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject guestbook "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.5.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler guestbook.handler/app
+         :init guestbook.handler/init
+         :destroy guestbook.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring/ring-mock "0.4.0"] [ring/ring-devel "1.7.1"]]}})
